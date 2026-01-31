5a3ca1c4e9203efbba4b2739b03625137f5167c5
Garik Asatryan <asa7ur@proton.me>
2026-01-31 16:01:23 +0100

    Adding new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {Home} from './features/home/home';
+import {Login} from './features/login/login';
+import {Regions} from './features/regions/regions';
+import {Forbidden} from './features/forbidden/forbidden';
+import {Error404} from './features/error404/error404';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'regions',
+    component: Regions,
+  },
+  {
+    path: 'forbidden',
+    component: Forbidden,
+  },
+  {
+    path: "**",
+    component: Error404,
+  }
+];
