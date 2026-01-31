3f8fc7e6b97b7d6fe966e16b512c19e4c8964c2e
Garik Asatryan <asa7ur@proton.me>
2026-01-31 16:01:33 +0100

    Adding new components

---
+++ src/app/features/regions/regions.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regions',
+  imports: [],
+  templateUrl: './regions.html',
+  styleUrl: './regions.css',
+})
+export class Regions {
+
+}
