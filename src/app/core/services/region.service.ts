1dec5d30c1b1a4041af490c7c0b77ef63ac63ee9
Garik Asatryan <asa7ur@proton.me>
2026-02-03 18:54:12 +0100

    Added pagination

---
+++ src/app/core/services/region.service.ts
@@ -0,0 +1,37 @@
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable, throwError} from 'rxjs';
+import {AuthService} from './auth';
+import {environment} from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RegionService {
+  constructor(private http: HttpClient, private authService: AuthService) {
+  }
+
+  /**
+   * Método para obtener las regiones desde la API.
+   * @returns Observable que emite la lista de regiones.
+   */
+  fetchRegions(page: number, size: number, sortColumn: string, sortDirection: string): Observable<any> {
+    const token = this.authService.getToken(); // Obtén el token del AuthService
+    if (!token) {
+      // Si no hay token, lanza un error indicando que el usuario no está autorizado.
+      return throwError(() => new Error('Unauthorized'));
+    }
+
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString())
+      .set('sort', `$(sortColumn),$(sortDirection)`);
+
+    // Realiza la solicitud GET al endpoint de regiones con el token en el encabezado.
+    return this.http.get(`${environment.apiUrl}/regions`, {
+      headers: new HttpHeaders({Authorization: `Bearer ${token}`}),
+      params: params
+    });
+  }
+}
--- src/app/features/regions/regions.ts
+++ src/app/features/regions/regions.ts
@@ -1,34 +1,87 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {RegionService} from '../../core/services/region';
+import {RegionService} from '../../core/services/region.service';
 import {Router} from '@angular/router';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatPaginator, MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+import {MatSort, MatSortModule, Sort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-regions',
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule],
   templateUrl: './regions.html',
   styleUrl: './regions.css',
 })
 export class Regions implements OnInit {
-  regions: any[] = [];
+  displayedColumns: string[] = ['id', 'name'];
+  dataSource = new MatTableDataSource<any>([]);
+  totalElements: number = 0;
+  totalPages: number = 0;
+  currentPage: number = 0;
+  pageSize: number = 10;
+  sortColumn: string = 'name';
+  sortDirection: string = 'asc';
   error: string | null = null;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private regionService: RegionService, private router: Router) {
+  regions: any[] = [];
+
+  constructor(
+    private regionService: RegionService,
+    private router: Router) {
   }
 
   ngOnInit() {
-    this.regionService.fetchRegions().subscribe({
-      next: (data: any) => {
-        this.regions = data.content;
-        console.log('Regiones cargadas:', this.regions);
+    this.fetchRegions(this.currentPage, this.pageSize, this.sortColumn, this.sortDirection);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
+    this.sort.sortChange.subscribe((sort: Sort) => this.handleSortEvent(sort));
+  }
+
+  fetchRegions(page: number, size: number, sortColumn: string, sortDirection: string) {
+    console.log(`Llamada al servicio con página: ${page}, tamaño: ${size}, orden: ${sortColumn} ${sortDirection}`);
+
+    this.regionService.fetchRegions(page, size, sortColumn, sortDirection).subscribe({
+      next: (res: any) => {
+        this.dataSource.data = res.content;
+        this.totalElements = res.totalElements;
+        this.totalPages = res.totalPages;
+        this.currentPage = res.number;
+        this.pageSize = res.size;
+
+        setTimeout(() => {
+          this.paginator.length = this.totalElements;
+          this.paginator.pageIndex = this.currentPage;
+          this.paginator.pageSize = this.pageSize;
+        });
       },
       error: (err) => {
+        console.error('Error al obtener datos: ', err);
+
         if (err.status === 403) {
           this.router.navigate(['/forbidden']);
         } else {
-          this.error = 'An error ocurred';
+          this.error = 'Error al cargar las regiones';
         }
-      }
+      },
     });
+  }
+
+  handleSortEvent(sort: Sort) {
+    console.log(`Cambio de orden detectado: Ordenado por ${sort.active} (${sort.direction})`);
+    this.sortColumn = sort.active;
+    this.sortDirection = sort.direction || 'asc';
+
+    this.fetchRegions(this.currentPage, this.pageSize, this.sortColumn, this.sortDirection);
+  }
+
+  handlePageEvent(event: PageEvent) {
+    console.log(`Cambio de página detectado: Página ${event.pageIndex}, Tamaño: ${event.pageSize}`);
+    this.fetchRegions(event.pageIndex, event.pageSize, this.sortColumn, this.sortDirection);
   }
 }
