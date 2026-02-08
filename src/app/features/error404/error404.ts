0ef371a943ca828b5eeb44bb2b20648df9896b1f
Garik Asatryan <asa7ur@proton.me>
2026-02-08 17:04:15 +0100

    Minor changes

--- src/app/features/error404/error404.ts
+++ src/app/features/error404/error404.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-error404',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './error404.html',
   styleUrl: './error404.css',
 })
